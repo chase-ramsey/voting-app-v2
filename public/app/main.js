8bef5623c2340f695cf60f9b81c84912e799b8e5
Chase Ramsey <ramsey.chase@gmail.com>
2016-06-09 13:53:51 -0500

    Initialize firebase

--- public/app/main.js
+++ public/app/main.js
@@ -1,4 +1,14 @@
 angular.module('app', [])
+  .config(() => {
+  // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyDDycs5gL0wt1NoQNTPRUGX6zcYWnGwhfY",
+      authDomain: "voting-app-exercise.firebaseapp.com",
+      databaseURL: "https://voting-app-exercise.firebaseio.com",
+      storageBucket: "voting-app-exercise.appspot.com",
+    };
+  firebase.initializeApp(config);
+  })
   .controller('MainCtrl', function() {
     const main = this;
 
